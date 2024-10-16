d30e1f9024017cea1ad9d106de6190e72f1e58b3
Valentin Bouin <vbouin@lefigaro.fr>
2024-10-16 17:54:40 +0200

    fix version

--- versions.tf
+++ versions.tf
@@ -1,14 +1,13 @@
 terraform {
-  required_version = ">= 1.5.5"
-
+  required_version = ">= 0.13"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 5.11.0"
+      version = ">= 4.53, < 7"
     }
+
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.26, < 5.15.0"
+      version = ">= 4.53, < 7"
     }
   }
-}
